9d11e6f3cd719aee8bdf94a87e0b2ca96897b689
Mateus <mateus_sat@hotmail.com>
2019-07-16 19:36:54 -0300

    double for

--- js/vetor2.js
+++ js/vetor2.js
@@ -1,3 +1,19 @@
 
+// criação de um vetor de vetor
 
 let alunos = new Array(2);
+
+alunos[0] = new Array(2);
+alunos[1] = new Array(2);
+
+
+alunos[0][0] = 'Chico';
+alunos[0][1] = 8;
+alunos[1][0] = 'Zé';
+alunos[1][1] = 5;
+
+for (let i = 0; i < alunos.length; i++) {
+
+    alert(alunos[i]);
+
+}
