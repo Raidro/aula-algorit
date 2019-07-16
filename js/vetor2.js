4eaa870a43696d43f28ae609a9bb8ff26ce72b86
Mateus <mateus_sat@hotmail.com>
2019-07-16 20:00:39 -0300

    preenchimento de vetor com for

--- js/vetor2.js
+++ js/vetor2.js
@@ -1,23 +1,46 @@
 
 // criação de um vetor de vetor
 
-let alunos = new Array(2);
+let alunos = new Array(5);
 
-alunos[0] = new Array(2);
-alunos[1] = new Array(2);
+// alunos[0] = new Array(2);
+// alunos[1] = new Array(2);
 
 
-alunos[0][0] = 'Chico';
-alunos[0][1] = 8;
-alunos[1][0] = 'Zé';
-alunos[1][1] = 5;
+// alunos[0][0] = 'Chico';
+// alunos[0][1] = 8;
+// alunos[1][0] = 'Zé';
+// alunos[1][1] = 5;
+
+for (let i = 0; i < alunos.length; i++) {
+
+    alunos[i] = new Array(2);
+
+    alunos[i][0] = prompt('Informe o Nome do aluno: ' + i);
+    alunos[i][1] = prompt('Informe a Nota do aluno: ' + i);
+
+}
 
 for (let i = 0; i < alunos.length; i++) {
     for (let j = 0; j < alunos[i].length; j++) {
-
         alert(alunos[i][j]);
-
     }
 
-
 }
+
+
+
+
+
+
+
+// for (let i = 0; i < alunos.length; i++) {
+
+//     for (let j = 0; j < alunos[i].length; j++) {
+
+//         alert(alunos[i][j]);
+
+//     }
+
+
+// }
