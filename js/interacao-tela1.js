45bb0a612a950a2ff4f7d63eb87e5d5cbb7e1a7c
Mateus <mateus_sat@hotmail.com>
2019-07-18 20:41:45 -0300

    correção do numero2

--- js/interacao-tela1.js
+++ js/interacao-tela1.js
@@ -1,16 +1,14 @@
 
-
-
 function calcular() {
 
     //é um procedimento pois não retorna nada/ uma função retorna algo
     let numero1 = document.getElementById('valor1').value;
     //let numero1 = parseInt(document.getElementById('valor1').value);
-    numero1 = validaCampo(numero1, 'valor1');
+    numero1 = validaCampo(numero1, 'mensagem1', 'Campo Invalido! Tem que ser numero');
 
     let numero2 = document.getElementById('valor2').value;
     //let numero2 = parseInt(document.getElementById('valor2').value);
-    numero1 = validaCampo(numero2, 'valor2');
+    numero2 = validaCampo(numero2, 'mensagem2', 'Campo Invalido! Tem que ser numero');
 
     let resultado = numero1 + numero2;
 
@@ -21,14 +19,15 @@ function ehNumero(numero) {
     return !isNaN(numero);
 }
 
-function validaCampo(valor, id) {
+function validaCampo(valor, id, mensagem) {
 
     if (ehNumero(valor)) {
-        valor = parseInt(valor);
-        return valor;
+        return parseInt(valor);
+
     } else {
-        let mensagem = id + 'deve ser numero'
-        document.getElementById('mensagem').innerHTML = mensagem;// innerHTML é entre as tags
+        //let mensagem = id + 'deve ser numero'
+        document.getElementById(id).style.color = 'red';
+        document.getElementById(id).innerHTML = mensagem;// innerHTML é entre as tags
     }
     return 0;
 
